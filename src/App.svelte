ca8bb3779cc9c6adc8f5f539bd122c84e9f71c33
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-06-20 20:03:24 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,23 +2,44 @@
   import { onMount } from 'svelte';
   import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-  
-
+  let stringAfterLastSlash  = ''; // Replace with the desired form ID
   let fields = [];
+  let currentURL = '';
+
+  onMount(() => {
+    currentURL = window.location.href;
+  });
+
+  function extractStringAfterLastSlash(url) {
+    const lastIndex = url.lastIndexOf('/');
+    return url.substring(lastIndex + 1);
+  }
+
+  $: {
+    stringAfterLastSlash = extractStringAfterLastSlash(currentURL);
+  }
 
   const fetchData = async () => {
-    const url = `https://api.recruitly.io/api/candidateform/details?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77`;
+    const url = `https://api.recruitly.io/api/candidateform/details/${stringAfterLastSlash}?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77`;
 
     try {
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error('Request failed');
       }
-      const data = await response.json();
-      console.log('Data:', data);
-      if (Array.isArray(data.fields)) {
-        fields = data.fields;
+      const data = await response.text();
+
+      // Check if the response is empty
+      if (data.trim() === '') {
+        throw new Error('Empty response');
+      }
+
+      const jsonData = JSON.parse(data);
+
+      console.log('Data:', jsonData);
+
+      if (Array.isArray(jsonData.fields)) {
+        fields = jsonData.fields;
       } else {
         throw new Error('Response data does not contain the "fields" property');
       }
@@ -41,36 +62,35 @@
 {#if fields.length > 0}
   <h2>Form Fields:</h2>
   <form>
-  
     {#each fields as field}
       {#if field.name === 'CV/Resume'}
         <div class="form-group">
           <label for="cvResume">{field.label}</label>
-          <input class="form-control-file" type="file" name={field.code} accept=".pdf,.doc,.docx">
+          <input class="form-control-file" type="file" id="cvResume" name={field.code} accept=".pdf,.doc,.docx">
         </div>
       {/if}
       {#if field.name === 'email'}
         <div class="form-group">
           <label for="email">{field.label}</label>
-          <input class="form-control" type="email"  name={field.code}>
+          <input class="form-control" type="email" id="email" name={field.code}>
         </div>
       {/if}
       {#if field.name === 'mobile'}
         <div class="form-group">
           <label for="mobile">{field.label}</label>
-          <input class="form-control" type="tel"  name={field.code}>
+          <input class="form-control" type="tel" id="mobile" name={field.code}>
         </div>
       {/if}
       {#if field.name === 'address.addressLine'}
         <div class="form-group">
           <label for="addressLine">{field.label}</label>
-          <input class="form-control" type="text"  name={field.code}>
+          <input class="form-control" type="text" id="addressLine" name={field.code}>
         </div>
       {/if}
       {#if field.code === 'FULL_NAME'}
         <div class="form-group">
           <label for="fullName">{field.label}</label>
-          <input class="form-control" type="text"  name={field.code}>
+          <input class="form-control" type="text" id="fullName" name={field.code}>
         </div>
       {/if}
     {/each}
@@ -79,3 +99,4 @@
 {:else}
   <p>Loading...</p>
 {/if}
+<p>The form ID from the URL is: {stringAfterLastSlash }</p>
