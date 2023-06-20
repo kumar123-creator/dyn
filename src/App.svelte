d7571430a7857fd7e5857dff076fba1700fc4570
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-06-20 18:01:30 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,3 @@
-
 <script>
   import { onMount } from 'svelte';
   import 'bootstrap/dist/css/bootstrap.min.css';
@@ -34,14 +33,38 @@
   <h2>Form Fields:</h2>
   <form>
     {#each fields as field}
-      <div class="form-group">
-        <label for={field.code}>{field.label}</label>
-        {#if field.code === 'CV'}
+      {#if field.name === 'CV/Resume'}
+        <div class="form-group">
+          <label for={field.code}>{field.label}</label>
           <input class="form-control-file" type="file" id={field.code} name={field.code} accept=".pdf,.doc,.docx">
-        {:else}
+        </div>
+      {/if}
+      {#if field.name === 'email'}
+        <div class="form-group">
+          <label for={field.code}>{field.label}</label>
+          <input class="form-control" type="email" id={field.code} name={field.code}>
+        </div>
+      {/if}
+      {#if field.name === 'mobile'}
+        <div class="form-group">
+          <label for={field.code}>{field.label}</label>
+          <input class="form-control" type="tel" id={field.code} name={field.code}>
+        </div>
+      {/if}
+      {#if field.name === 'address.addressLine'}
+        <div class="form-group">
+          <label for={field.code}>{field.label}</label>
           <input class="form-control" type="text" id={field.code} name={field.code}>
-        {/if}
-      </div>
+        </div>
+      {/if}
+      {#if field.code=== 'FULL_NAME'}
+        <div class="form-group">
+          <label for={field.code}>{field.label}</label>
+          <input class="form-control" type="text" id={field.code} name={field.code}>
+        </div>
+      {/if}
+     
+      
     {/each}
     <button class="btn btn-primary" type="submit">Submit</button>
   </form>
