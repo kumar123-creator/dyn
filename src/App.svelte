06b9cd4f6a049cb7818314893e5474a14ea9a30b
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-06-20 09:56:20 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,54 @@
 <script>
   import { onMount } from 'svelte';
+  import 'bootstrap/dist/css/bootstrap.min.css'
 
-  let formFields = [
-    { label: 'Fullname', value: '' },
-    { label: 'Email', value: '' },
-    { label: 'Mobile', value: '' },
-    { label: 'Address', value: '' }
-  ];
+  let formFields = [];
+  let formId = '72fbc0da-3810-4ad9-a922-1845f8974eb7';
+  let firstName = '';
+  let email = '';
+  let mobile = '';
+ 
+
+  function fetchData() {
+    const formData = {
+      formId,
+    };
+    function fetchData() {
+  fetch(`https://api.recruitly.io/api/candidateform/details/${formId}?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77`)
+    .then(response => response.json())
+    .then(data => {
+      console.log('API Response:', data);
+      firstName = data.firstName;
+      email = data.email;
+      mobile = data.mobile;
+      // Update the form fields based on the fetched data
+      formFields = Object.entries(data.formFields).map(([label, value]) => ({ label, value }));
+    })
+    .catch(error => {
+      console.error('API Error:', error);
+    });
+}
+
+  }
 
   function handleSubmit() {
-    const formData = {};
-    formFields.forEach(field => {
-      formData[field.label.toLowerCase()] = field.value;
-    });
-    window.dispatchEvent(new CustomEvent('submit', { detail: formData }));
+    // Clear the formFields array before populating it
+    formFields = [];
+
+    // Push the form input values to the formFields array
+    formFields.push({ label: 'firstName', value: firstName });
+    formFields.push({ label: 'Email', value: email });
+    formFields.push({ label: 'Mobile', value: mobile });
+
+    // Perform any necessary actions on form submission
+    console.log('Form Fields:', formFields);
+    
   }
+
+  onMount(() => {
+    // Fetch data when the component is mounted
+    fetchData();
+  });
 </script>
 
 <style>
@@ -34,7 +68,7 @@
 
 <div class="form-container">
   <form on:submit|preventDefault={handleSubmit}>
-    {#each formFields as field, index}
+    {#each formFields as field}
       <div class="form-group">
         <label for={field.label.toLowerCase()} class="form-label">{field.label}</label>
         <input type="text" id={field.label.toLowerCase()} class="form-control" bind:value={field.value} />
